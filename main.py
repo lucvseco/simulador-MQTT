ec22c89efff1636c62ba680417bb09dbe9ba763f
Limeira8 <134227798+Limeira8@users.noreply.github.com>
2023-09-25 10:43:59 -0300

    Mais detalhes (Chat GPT)
    Algumas classes com mais detalhes

--- main.py
+++ main.py
@@ -2,17 +2,87 @@
 
 class Car:
     def __init__(self, car_id, position, speed):
-        # Implemente a classe Car para representar os carros
+
+        self.car_id = car_id  # Identificador único do carro
+        self.current_position = initial_position  # Posição atual do carro (x, y)
+        self.current_speed = 0  # Velocidade atual do carro
+        self.max_speed = max_speed  # Velocidade máxima do carro
+        self.destination = None  # Destino atual do carro
+        self.lock = threading.Lock()  # Lock para proteger o acesso a dados do carro
+        self.is_parked = False  # Indica se o carro está estacionado
+
+    def set_position(self, position):
+        with self.lock:
+            self.current_position = position
+
+    def set_speed(self, speed):
+        with self.lock:
+            self.current_speed = speed
+
+    def set_destination(self, destination):
+        with self.lock:
+            self.destination = destination
+
+    def park(self):
+        with self.lock:
+            self.is_parked = True
+
+    def unpark(self):
+        with self.lock:
+            self.is_parked = False
+
+    def is_at_destination(self):
+        with self.lock:
+            return self.current_position == self.destination
+
+    def move_towards_destination(self):
+        # Implemente a lógica para mover o carro em direção ao destino
         pass
 
+    def update(self):
+        # Implemente a lógica de atualização periódica do carro
+        pass
+        
 class Person:
-    def __init__(self, person_id, position):
-        # Implemente a classe Person para representar as pessoas
+   def __init__(self, person_id, initial_position):
+        self.person_id = person_id  # Identificador único da pessoa
+        self.current_position = initial_position  # Posição atual da pessoa (x, y)
+        self.destination = None  # Destino atual da pessoa
+        self.lock = threading.Lock()  # Lock para proteger o acesso a dados da pessoa
+
+    def set_position(self, position):
+        with self.lock:
+            self.current_position = position
+
+    def set_destination(self, destination):
+        with self.lock:
+            self.destination = destination
+
+    def is_at_destination(self):
+        with self.lock:
+            return self.current_position == self.destination
+
+    def move_towards_destination(self):
+        # Implemente a lógica para mover a pessoa em direção ao destino
+        pass
+
+    def update(self):
+        # Implemente a lógica de atualização periódica da pessoa
         pass
 
 class Street:
     def __init__(self, street_id, start_point, end_point, width):
-        # Implemente a classe Street para representar as ruas
+        self.street_id = street_id  # Identificador único da rua
+        self.start_point = start_point  # Coordenadas do ponto inicial da rua (x, y)
+        self.end_point = end_point  # Coordenadas do ponto final da rua (x, y)
+        self.width = width  # Largura da rua
+        self.lock = threading.Lock()  # Lock para proteger o acesso a dados da rua
+
+    def get_length(self):
+        with self.lock:
+            # Calcula o comprimento da rua com base nas coordenadas dos pontos inicial e final
+            return math.sqrt((self.end_point[0] - self.start_point[0])**2 +
+                             (self.end_point[1] - self.start_point[1])**2)
         pass
 
 class CentralControl:
@@ -34,11 +104,75 @@ class CentralControl:
 
     def receive_transport_request(self, person, destination):
         # Implemente a lógica para receber solicitações de transporte das pessoas
+        passdef __init__(self):
+        self.cars = []  # Lista de carros na simulação
+        self.people = []  # Lista de pessoas na simulação
+        self.streets = []  # Lista de ruas na simulação
+        self.lock = threading.Lock()  # Lock para proteger o acesso a dados do controle central
+
+    def add_car(self, car):
+        with self.lock:
+            self.cars.append(car)
+
+    def add_person(self, person):
+        with self.lock:
+            self.people.append(person)
+
+    def add_street(self, street):
+        with self.lock:
+            self.streets.append(street)
+
+    def receive_person_message(self, message):
+        # Implemente a lógica para receber mensagens das pessoas
+        pass
+
+    def receive_car_message(self, message):
+        # Implemente a lógica para receber mensagens dos carros
+        pass
+
+    def receive_destination(self, car, destination):
+        with self.lock:
+            car.set_destination(destination)
+
+    def receive_transport_request(self, person, destination):
+        # Implemente a lógica para receber solicitações de transporte das pessoas
+        pass
+
+    def update(self):
+        # Implemente a lógica de atualização periódica do controle central
         pass
 
 class CommunicationManager:
     def __init__(self):
-        # Implemente a classe CommunicationManager para gerenciar a comunicação
+        # Inicialize os recursos necessários para a comunicação, como conexões MQTT, se aplicável
+        pass
+
+    def register_car(self, car):
+        # Registre um carro na classe de comunicação
+        pass
+
+    def register_person(self, person):
+        # Registre uma pessoa na classe de comunicação
+        pass
+
+    def register_central_control(self, central_control):
+        # Registre o controle central na classe de comunicação
+        pass
+
+    def send_message(self, sender, recipient, message):
+        # Envie uma mensagem de um remetente para um destinatário
+        pass
+
+    def mqtt_publish(self, topic, payload):
+        # Implemente a publicação MQTT de mensagens
+        pass
+
+    def mqtt_subscribe(self, topic, callback):
+        # Implemente a assinatura MQTT de tópicos com um callback
+        pass
+
+    def call_function(self, function_name, args):
+        # Implemente a chamada de função para comunicação via chamada de função
         pass
 
     # Implemente métodos para comunicação MQTT e chamada de função, conforme necessário
