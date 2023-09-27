70acfd82d34f2ba9d1232559fc7e3dd781ceea4a
Limeira8 <134227798+Limeira8@users.noreply.github.com>
2023-09-27 19:49:53 -0300

    Update main.py
    Classes separadas

--- main.py
+++ main.py
@@ -1,204 +1,77 @@
-# Importe as bibliotecas necessárias, como MQTT para comunicação, bibliotecas de visualização de dados, etc.
+import random
+import time
 
-class Car:
-    def __init__(self, car_id, position, speed):
+class Pessoa:
+    def __init__(self, nome, posicao_atual, destino):
+        self.nome = nome
+        self.posicao_atual = posicao_atual
+        self.destino = destino
 
-        self.car_id = car_id  # Identificador único do carro
-        self.current_position = initial_position  # Posição atual do carro (x, y)
-        self.current_speed = 0  # Velocidade atual do carro
-        self.max_speed = max_speed  # Velocidade máxima do carro
-        self.destination = None  # Destino atual do carro
-        self.lock = threading.Lock()  # Lock para proteger o acesso a dados do carro
-        self.is_parked = False  # Indica se o carro está estacionado
+class Carro:
+    def __init__(self, placa):
+        self.placa = placa
+        self.velocidade = 0
+        self.posicao = (0, 0)
 
-    def set_position(self, position):
-        with self.lock:
-            self.current_position = position
+    def mover(self, nova_posicao, nova_velocidade):
+        self.posicao = nova_posicao
+        self.velocidade = nova_velocidade
 
-    def set_speed(self, speed):
-        with self.lock:
-            self.current_speed = speed
-
-    def set_destination(self, destination):
-        with self.lock:
-            self.destination = destination
-
-    def park(self):
-        with self.lock:
-            self.is_parked = True
-
-    def unpark(self):
-        with self.lock:
-            self.is_parked = False
-
-    def is_at_destination(self):
-        with self.lock:
-            return self.current_position == self.destination
-
-    def move_towards_destination(self):
-        # Implemente a lógica para mover o carro em direção ao destino
-        pass
-
-    def update(self):
-        # Implemente a lógica de atualização periódica do carro
-        pass
-        
-class Person:
-   def __init__(self, person_id, initial_position):
-        self.person_id = person_id  # Identificador único da pessoa
-        self.current_position = initial_position  # Posição atual da pessoa (x, y)
-        self.destination = None  # Destino atual da pessoa
-        self.lock = threading.Lock()  # Lock para proteger o acesso a dados da pessoa
-
-    def set_position(self, position):
-        with self.lock:
-            self.current_position = position
-
-    def set_destination(self, destination):
-        with self.lock:
-            self.destination = destination
-
-    def is_at_destination(self):
-        with self.lock:
-            return self.current_position == self.destination
-
-    def move_towards_destination(self):
-        # Implemente a lógica para mover a pessoa em direção ao destino
-        pass
-
-    def update(self):
-        # Implemente a lógica de atualização periódica da pessoa
-        pass
-
-class Street:
-    def __init__(self, street_id, start_point, end_point, width):
-        self.street_id = street_id  # Identificador único da rua
-        self.start_point = start_point  # Coordenadas do ponto inicial da rua (x, y)
-        self.end_point = end_point  # Coordenadas do ponto final da rua (x, y)
-        self.width = width  # Largura da rua
-        self.lock = threading.Lock()  # Lock para proteger o acesso a dados da rua
-
-    def get_length(self):
-        with self.lock:
-            # Calcula o comprimento da rua com base nas coordenadas dos pontos inicial e final
-            return math.sqrt((self.end_point[0] - self.start_point[0])**2 +
-                             (self.end_point[1] - self.start_point[1])**2)
-        pass
-
-class CentralControl:
+class CentralControle:
     def __init__(self):
-        # Implemente a classe CentralControl para gerenciar o tráfego
-        pass
+        self.carros = []
+        self.pessoas = []
 
-    def receive_person_message(self, message):
-        # Implemente a lógica para receber mensagens das pessoas
-        pass
+    def cadastrar_carro(self, carro):
+        self.carros.append(carro)
 
-    def receive_car_message(self, message):
-        # Implemente a lógica para receber mensagens dos carros
-        pass
+    def cadastrar_pessoa(self, pessoa):
+        self.pessoas.append(pessoa)
 
-    def receive_destination(self, car, destination):
-        # Implemente a lógica para receber destinos dos carros
-        pass
+    def enviar_pedido(self, pessoa):
+        carro_disponivel = self.encontrar_carro_disponivel()
+        if carro_disponivel:
+            self.enviar_carro(carro_disponivel, pessoa)
 
-    def receive_transport_request(self, person, destination):
-        # Implemente a lógica para receber solicitações de transporte das pessoas
-        passdef __init__(self):
-        self.cars = []  # Lista de carros na simulação
-        self.people = []  # Lista de pessoas na simulação
-        self.streets = []  # Lista de ruas na simulação
-        self.lock = threading.Lock()  # Lock para proteger o acesso a dados do controle central
+    def enviar_carro(self, carro, pessoa):
+        # Implemente a lógica de cálculo de rota aqui
+        rota = calcular_rota(carro.posicao, pessoa.posicao)
+        carro.mover(pessoa.posicao, velocidade=60)
+        pessoa.destino = (random.randint(0, 100), random.randint(0, 100))  # Exemplo de destino aleatório
+        carro.mover(pessoa.destino, velocidade=60)
 
-    def add_car(self, car):
-        with self.lock:
-            self.cars.append(car)
+    def encontrar_carro_disponivel(self):
+        # Implemente a lógica para encontrar um carro disponível
+        carros_disponiveis = [carro for carro in self.carros if carro.velocidade == 0]
+        if carros_disponiveis:
+            return carros_disponiveis[0]
+        else:
+            return None
 
-    def add_person(self, person):
-        with self.lock:
-            self.people.append(person)
+def calcular_rota(origem, destino):
+    # Implemente a lógica de cálculo de rota aqui
+    return []  # Retornar a lista de coordenadas da rota
 
-    def add_street(self, street):
-        with self.lock:
-            self.streets.append(street)
-
-    def receive_person_message(self, message):
-        # Implemente a lógica para receber mensagens das pessoas
-        pass
-
-    def receive_car_message(self, message):
-        # Implemente a lógica para receber mensagens dos carros
-        pass
-
-    def receive_destination(self, car, destination):
-        with self.lock:
-            car.set_destination(destination)
-
-    def receive_transport_request(self, person, destination):
-        # Implemente a lógica para receber solicitações de transporte das pessoas
-        pass
-
-    def update(self):
-        # Implemente a lógica de atualização periódica do controle central
-        pass
-
-class CommunicationManager:
-    def __init__(self):
-        # Inicialize os recursos necessários para a comunicação, como conexões MQTT, se aplicável
-        pass
-
-    def register_car(self, car):
-        # Registre um carro na classe de comunicação
-        pass
-
-    def register_person(self, person):
-        # Registre uma pessoa na classe de comunicação
-        pass
-
-    def register_central_control(self, central_control):
-        # Registre o controle central na classe de comunicação
-        pass
-
-    def send_message(self, sender, recipient, message):
-        # Envie uma mensagem de um remetente para um destinatário
-        pass
-
-    def mqtt_publish(self, topic, payload):
-        # Implemente a publicação MQTT de mensagens
-        pass
-
-    def mqtt_subscribe(self, topic, callback):
-        # Implemente a assinatura MQTT de tópicos com um callback
-        pass
-
-    def call_function(self, function_name, args):
-        # Implemente a chamada de função para comunicação via chamada de função
-        pass
-
-    # Implemente métodos para comunicação MQTT e chamada de função, conforme necessário
-
-class TrafficSimulation:
-    def __init__(self):
-        # Implemente a classe TrafficSimulation para controlar a simulação
-        pass
-
-    def configure_simulation(self, num_people, num_cars, num_streets, config_data):
-        # Implemente a configuração da simulação com base nos parâmetros especificados
-        pass
-
-    def start_simulation(self):
-        # Implemente a lógica para iniciar a simulação
-        pass
-
-    def stop_simulation(self):
-        # Implemente a lógica para parar a simulação
-        pass
-
-    def visualize_data(self):
-        # Implemente a lógica para visualizar os dados da simulação
-        pass
-
-# Implemente a lógica principal para configurar e iniciar a simulação
 if __name__ == "__main__":
-    # Configurar e iniciar a simulação
-    pass
+    central = CentralControle()
+
+    # Cadastrar carros
+    for i in range(5):
+        carro = Carro(id=i)
+        central.cadastrar_carro(carro)
+
+    # Cadastrar pessoas
+    for i in range(10):
+        pessoa = Pessoa(nome=f"Pessoa {i}", posicao_atual=(random.randint(0, 100), random.randint(0, 100)), destino=None)
+        central.cadastrar_pessoa(pessoa)
+        central.enviar_pedido(pessoa)
+
+    # Simulação de movimento dos carros
+    while True:
+        for carro in central.carros:
+            if carro.velocidade > 0:
+                # Simulação de movimento
+                nova_posicao = (carro.posicao[0] + 1, carro.posicao[1] + 1)  # Exemplo de movimento simples
+                carro.mover(nova_posicao, nova_velocidade=60)
+                print(f"Carro {carro.id} se moveu para {nova_posicao}")
+        time.sleep(1)
